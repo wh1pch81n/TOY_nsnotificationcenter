d6ad03a27b83244a396712117c5b8adf62cd3233
wh1pch81n <wh1pch81n@gmail.com>
2014-08-05 18:06:00 -0700

    created a custom view with a framework.  you can add properties to the IBInspector and modify them by hand rather then in code

---
+++ CustomView/CustomView.h
@@ -0,0 +1,19 @@
+//
+//  CustomView.h
+//  CustomView
+//
+//  Created by Derrick Ho on 8/5/14.
+//  Copyright (c) 2014 dnthome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomView.
+FOUNDATION_EXPORT double CustomViewVersionNumber;
+
+//! Project version string for CustomView.
+FOUNDATION_EXPORT const unsigned char CustomViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomView/PublicHeader.h>
+
+
---
+++ TOY_nsnotification/toyView.swift
@@ -0,0 +1,80 @@
+//
+//  toyView.swift
+//  TOY_nsnotification
+//
+//  Created by Derrick Ho on 8/5/14.
+//  Copyright (c) 2014 dnthome. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class ToyView: UIView {
+    @IBInspectable var cornerRadius:CGFloat = 0 {
+    didSet {
+        layer.cornerRadius = cornerRadius;
+    }
+    }
+    @IBInspectable var borderWidth:CGFloat = 0 {
+    didSet {
+        layer.borderWidth = borderWidth;
+    }
+    }
+    @IBInspectable var borderColor:UIColor = UIColor.clearColor() {
+    didSet {
+        layer.borderColor = borderColor.CGColor;
+    }
+    }
+
+    @IBInspectable var notifyButtonFrame:CGRect = CGRectMake(0, 0, 10, 10) {
+    didSet {
+        self.notifyButton.frame = notifyButtonFrame;
+    }
+    }
+    @IBInspectable var textListenerFrame:CGRect = CGRectMake(0, 0, 10, 10) {
+    didSet {
+        self.textListener.frame = textListenerFrame;
+    }
+    }
+    @IBInspectable var subviewListenerFrame:CGRect = CGRectMake(0, 0, 10, 10) {
+    didSet {
+        self.subViewListener.frame = subviewListenerFrame;
+    }
+    }
+
+    
+    
+    var notifyButton:UIButton = UIButton();
+    var textListener:UILabel = UILabel();
+    var subViewListener:UIView = UIView();
+    
+    init(frame: CGRect) {
+//        self.notifyButton = UIButton();
+//        self.textListener = UILabel();
+//       self.subViewListener = UIView();
+//        self.cornerRadius = 0;
+//        self.borderColor = UIColor.clearColor();
+//        self.borderWidth = 0;
+        
+        
+        super.init(frame: frame);
+    }
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder);
+    }
+    
+    override func drawRect(rect: CGRect) {
+//        self.notifyButton.frame = CGRectMake(0, 0, 10, 10);
+//        self.textListener.frame = CGRectMake(0, 50, 10, 10);
+//        self.subViewListener.frame = CGRectMake(0, 100, 10, 10);
+
+        self.notifyButton.backgroundColor = UIColor.greenColor();
+        self.textListener.backgroundColor = UIColor.yellowColor();
+        self.subViewListener.backgroundColor = UIColor.redColor();
+        
+        self.addSubview(self.notifyButton);
+        self.addSubview(self.textListener);
+        self.addSubview(self.subViewListener);
+    }
+}
